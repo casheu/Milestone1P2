732eac50fff5c042840fbfe220c18bcf4ff90e3e
Huesca Rashad <114060945+casheu@users.noreply.github.com>
2022-12-02 11:18:20 +0700

    initial

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import joblib
 app = Flask(__name__)
 
 # Load Pipeline Model
-with open('final_pipeline.pkl', 'rb') as file_1:
-  model_pipeline = joblib.load(file_1)
+with open('pipeline.pkl', 'rb') as file_1:
+  pipeline = joblib.load(file_1)
 
 # Load Sequential Model
 from tensorflow.keras.models import load_model
-model_seq = load_model('titanic_model.h5')
+model_seq = load_model('model.h5')
 
 @app.route("/")
 def home():
@@ -23,32 +23,41 @@ def titanic_predict():
     args = request.json
 
     data_inf = {
-      'PassengerId': args.get('PassengerId'),
-      'Pclass': args.get('Pclass'), 
-      'Name': args.get('Name'), 
-      'Sex': args.get('Sex'), 
-      'Age': args.get('Age'), 
-      'SibSp': args.get('SibSp'),
-      'Parch':  args.get('Parch'), 
-      'Ticket': args.get('Ticket'), 
-      'Fare': args.get('Fare'), 
-      'Cabin': args.get('Cabin'), 
-      'Embarked': args.get('Embarked')
+      'customerID': args.get('customerID'),
+      'gender': args.get('gender'), 
+      'SeniorCitizen': args.get('SeniorCitizen'), 
+      'Partner': args.get('Partner'), 
+      'Dependents': args.get('Dependents'),
+      'PhoneService': args.get('PhoneService'), 
+      'MultipleLines': args.get('MultipleLines'), 
+      'InternetService': args.get('InternetService'), 
+      'OnlineSecurity': args.get('OnlineSecurity'), 
+      'OnlineBackup': args.get('OnlineBackup'),
+      'DeviceProtection': args.get('DeviceProtection'),
+      'TechSupport': args.get('TechSupport'), 
+      'StreamingTV': args.get('StreamingTV'), 
+      'StreamingMovies': args.get('StreamingMovies'), 
+      'tenure': args.get('tenure'),
+      'Contract': args.get('Contract'), 
+      'PaperlessBilling': args.get('PaperlessBilling'), 
+      'PaymentMethod': args.get('PaymentMethod'), 
+      'MonthlyCharges': args.get('MonthlyCharges'), 
+      'TotalCharges': args.get('TotalCharges')
     }
     print('[DEBUG] Data Inference : ', data_inf)
     
     # Transform Inference-Set
     data_inf = pd.DataFrame([data_inf])
-    new_data_transform = model_pipeline.transform(data_inf)
+    new_data_transform = pipeline.transform(data_inf)
     new_data_transform
 
     # Predict using Neural Network
-    y_pred_inf = model_seq.predict(new_data_transform)
+    y_pred_inf = model.predict(new_data_transform)
     y_pred_inf = np.where(y_pred_inf >= 0.5, 1, 0)[0][0]
     if y_pred_inf == 0:
-      label = 'Not Survived'
+      label = 'Not Churned'
     else:
-        label = 'Survived'
+        label = 'Churned'
     
     print('[DEBUG] Result : ', y_pred_inf, label)
     print('')
